02435a8ac1a95dbf8ead799deaae29b70044afa2
Gabor Igloi <gabor.igloi@citrix.com>
2018-05-15 11:26:47 +0100

    Add another function to example
    Signed-off-by: Gabor Igloi <gabor.igloi@citrix.com>

--- examples/test.ml
+++ examples/test.ml
@@ -3,6 +3,7 @@ module Interface(R: Idl.RPC) = struct
   open R
 
   let str = Idl.Param.mk Rpc.Types.string
+  let int_p = Idl.Param.mk Rpc.Types.int
 
   let concat =
     declare
@@ -10,12 +11,19 @@ module Interface(R: Idl.RPC) = struct
       ["Concatenates the inputs"]
       (str @-> str @-> returning str Idl.DefaultError.err)
 
+  let add =
+    declare
+      "add"
+      ["Adds two numbers"]
+      (int_p @-> int_p @-> returning int_p Idl.DefaultError.err)
+
   let description = Idl.Interface.{
       name = "Test interface";
       namespace = None;
       description =
-        [ "Test interface that has a function"
-        ; "for concatenating the inputs" ];
+        [ "Test interface that has a function "
+        ; "for concatenating the inputs "
+        ; "and another for adding numbers" ];
       version=(1,0,0)
     }
 
@@ -27,4 +35,5 @@ let () =
   let module RPC = Rpc_tk.Guigen.GenGui () in
   let module Gui = Interface(RPC) in
   Gui.concat (fun a b -> a ^ b);
+  Gui.add (fun a b -> a + b);
   Rpc_tk.Guigen.creategui Gui.description Gui.implementation
